623c57c7240c03aa72c756f67400a0b31f7ab96e
Yeqi <34756296+weiyeqi@users.noreply.github.com>
2025-09-02 20:35:57 +0000

    Lecture Coding Example

--- MyApp/main.c
+++ MyApp/main.c
@@ -1,8 +1,24 @@
-#include <stdio.h>
+/*
+Lecture: week02-2
+Author: Yeqi Wei
+*/
+#include <stdio.h>   // include header files: stdio.h, meaning standard input/output header file.
+#define TENYEAR 10
+#define TENYEARAGO(x) x - 10
 
 int main(void) {
+   int year;
+   printf("Enter the year: ");
+   scanf("%d", &year);
+   printf("This year is: %d.\n", year);   // print the variable year
+   printf("10 years ago is: %d.\n", year - 10);
+   printf("10 years ago is: %d.\n", year - TENYEAR);
+   printf("10 years ago is: %d.\n", TENYEARAGO(year));
 
-   /* Your code here */
-
+   printf("The address of variable year: %p.\n", &year); 
+   /*
+   &year means the address/reference/pointer of variable year;
+   %p, meaning formatted the &year into a pointer format.
+   */
    return 0;
 }
